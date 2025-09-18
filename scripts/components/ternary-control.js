95554f07d428002528a52bbbc973c4fa882964de
Fodor Attila <fodor.attila@steradian.hu>
2025-09-18 20:13:47 +0200

    Calculation service is working (with some known bugs...)

--- scripts/components/ternary-control.js
+++ scripts/components/ternary-control.js
@@ -9,38 +9,28 @@ export class TernaryControl extends LitElement {
             margin: 16px 0 0 0;
         }
 
-        .blue
-        {
-            border-left: 4px solid #3700dd;
-        }
-        .purple
-        {
-            border-left: 4px solid #7800c0;
-        }
+        .blue { border-left: 4px solid #3700dd; }
+        .purple { border-left: 4px solid #7800c0; }
         
-        .title
-        {
-            font-weight: bold;
-        }
+        .title { font-weight: bold; }
+        
+        ul.radix { margin: 0; }
+        ul.radix li { list-style: none; }
     `;
 
     static properties = {
         name: { type: String },
-        direction: { type: String },
-        operandLeft: { type: Number },
-        operandRight: { type: Number }
+        direction: { type: String }
     }
 
     constructor() {
         super();
         this.name = 'ternary';
         this.direction = "output";
-        this.operandLeft = 0;
-        this.operandRight = 0;
-        this.latest = 0;
+        this.latest = {};
     }
 
-    convert() {
+    convertValue() {
         let input = this.renderRoot.querySelector("#input");
         let decNumber = Number(input.value);
 
@@ -48,13 +38,17 @@ export class TernaryControl extends LitElement {
         for (let t of ternaryNumbers)
             t.decValue = decNumber;
 
-        calcService.data = decNumber;
+        calcService.setProperty(this.name, decNumber);
+    }
+
+    setOperator() {
+        let selectElement = this.renderRoot.querySelector("#operator");
+        let selectedValue = selectElement.selectedOptions[0].text
+        calcService.setProperty("Operator", selectedValue);
     }
 
     execute() {
-        let ternaryNumbers = this.renderRoot.querySelectorAll("ternary-number");
-        for (let t of ternaryNumbers)
-            t.decValue = calcService.data;
+        calcService.execute();
     }
 
     connectedCallback() {
@@ -68,8 +62,9 @@ export class TernaryControl extends LitElement {
     }
 
     _onData = (e) => {
-        this.latest = e.detail.value;
-        this.requestUpdate();
+        this.latest = e.detail;
+        if (this.direction === "output")
+            this.requestUpdate();
     };
 
     render() {
@@ -79,8 +74,8 @@ export class TernaryControl extends LitElement {
             ${this.direction.startsWith("input") ?
                 html`<label for="input">DEC:</label>
                     <input type="text" id="input" name="input" value="0" />
-                    <button @click=${this.convert}>Convert</button>` :
-                html`<label for="input">Operator:</label>
+                    <button @click=${this.convertValue}>Convert</button>` :
+                html`<label for="operator">Operator:</label>
                     <select id="operator" name="operator">
                         <option selected>None</option>
                         <option>Add (+)</option>
@@ -89,12 +84,18 @@ export class TernaryControl extends LitElement {
                         <option>Div (/)</option>
                         <option>Rem (%)</option>
                     </select>
+                    <button @click=${this.setOperator}>Set</button><br />
+                    Execute calculation in
+                    <ul class="radix">
+                        <li><input type="radio" name="radix" value="dec" checked /> Decimal</li>
+                        <li><input type="radio" name="radix" value="ter" disabled /> Ternary (Soon...)</li>
+                    </ul>
                     <button @click=${this.execute}>Execute</button>`
         }
         <div class="strip ${color}">
-            <ternary-number class="${this.name}" color="${color}" decValue="${this.latest}" balanced></ternary-number>
-            <ternary-number class="${this.name}" color="${color}" decValue="${this.latest}"></ternary-number>
-            <ternary-number class="${this.name}" color="${color}" decValue="${this.latest}" showComplement></ternary-number>
+            <ternary-number class="${this.name}" color="${color}" decValue="${this.latest[this.name]}" balanced></ternary-number>
+            <ternary-number class="${this.name}" color="${color}" decValue="${this.latest[this.name]}"></ternary-number>
+            <ternary-number class="${this.name}" color="${color}" decValue="${this.latest[this.name]}" showComplement></ternary-number>
         </div>`;
     }
 }
--- scripts/services/calc-service.js
+++ scripts/services/calc-service.js
@@ -1,7 +1,5 @@
-import {LitElement, html} from "lit";
-
 export class CalcService extends EventTarget {
-    #data = 1000;
+    #data = {};
 
     get data() {
         return this.#data;
@@ -9,11 +7,46 @@ export class CalcService extends EventTarget {
 
     set data(value) {
         if (this.#data !== value) {
+            console.log(value);
             this.#data = value;
-            const doubled = this.#data * 2;
             this.dispatchEvent(new CustomEvent("data-changed", {
-                detail: { value: this.#data, doubled }
+                detail: this.#data
             }));
         }
+    }
+
+    setProperty(property, value) {
+        // Direct access to a property of #data wouldn't trigger the setter's event.
+        const local = structuredClone(this.data);
+        local[property] = value;
+        this.data = local;
+    }
+
+    execute() {
+        const requiredKeys = ["Operand Left", "Operand Right", "Operator"];
+        const allExistAndValid = requiredKeys.every(
+            key => Object.hasOwn(this.#data, key) && typeof this.#data[key] !== "undefined"
+        );
+
+        if (!allExistAndValid || this.#data["Operator"] === "None") {
+            this.setProperty("Result", 0);
+            return false;
+        }
+
+        const operators = {
+            "Add (+)": (a, b) => a + b,
+            "Sub (-)": (a, b) => a - b,
+            "Mul (*)": (a, b) => a * b,
+            "Div (/)": (a, b) => a / b,
+            "Rem (%)": (a, b) => a % b
+        };
+
+        this.result = operators[this.#data["Operator"]](
+            Number(this.#data["Operand Left"]),
+            Number(this.#data["Operand Right"]));
+
+
+        this.setProperty("Result", this.result);
+        return true;
     }
 }
